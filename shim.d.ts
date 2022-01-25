2b74d0c3a52865ad0d5c9d2afe4120f072ac18e3
jeffjing <zgayjjf@qq.com>
2022-01-25 15:25:50 +0800

    fix: fix broken type of webext-bridge

--- shim.d.ts
+++ shim.d.ts
@@ -1,6 +1,6 @@
-import { ProtocolWithReturn } from '~/libs/webext-bridge'
+import { ProtocolWithReturn } from '~~/libs/webext-bridge'
 
-declare module '~/libs/webext-bridge' {
+declare module '~~/libs/webext-bridge' {
   export interface ProtocolMap {
     // define message protocol types
     // see https://github.com/antfu/webext-bridge#type-safe-protocols
