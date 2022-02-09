8d3dcf0a96c722bcdb71cd10bd25ebc16aa4761e
jeffjing <zgayjjf@qq.com>
2022-02-09 18:14:39 +0800

    refactor: extract BoxList & BoxItem

--- src/ui/popup/pages/addAddress/selectWays.vue
+++ src/ui/popup/pages/addAddress/selectWays.vue
@@ -1,75 +1,44 @@
 <style lang="scss" scoped>
 .page-add-address {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  .key-box {
-    padding: 82px 16px;
-    .key-item-box {
-      width: 100%;
-      height: 66px;
-      padding: 15px 16px 15px 10px;
-      border-radius: 12px;
-      border: 1px solid rgba(191, 191, 191, 0.09);
-      display: flex;
-      align-items: center;
-      background: #F9F7F6;
-      cursor: pointer;
-      &:hover {
-        background: #F0EDED;
-      }
-      div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 36px;
-        height: 36px;
-        margin-right: 8px;
-      }
-      span {
-        font-size: $input-font-size;
-        font-weight: 500;
-      }
-      .arrow-right {
-        margin-left: auto;
-      }
-    }
-  }
 }
 </style>
 
 <template>
   <div class="page-add-address">
-    <UniTab :title="$tt('Select Ways')"></UniTab>
-    <div class="key-box">
-      <div v-if="hasMnemonic" class="key-item-box mb-[24px]" @click="onClickDeriveAddress">
-        <div>
+    <UniTab :title="$tt('Select Ways')" />
+
+    <BoxList :pad-top="82">
+      <BoxItem v-if="hasMnemonic" @click="onClickDeriveAddress">
+        <template #head>
           <Iconfont name="create" size="17" color="#6D8AF3" />
-        </div>
-        <span>{{ $tt('Derive From Mnemonic') }}</span>
-        <Iconfont class="arrow-right" name="arrow-right" width="12" height="14" color="#D8D8D8" />
-      </div>
-      <div v-else class="key-item-box mb-[24px]" @click="$router.push('/addAddress/createMnemonic')">
-        <div>
+        </template>
+
+        {{ $tt('Derive From Mnemonic') }}
+      </BoxItem>
+
+      <BoxItem v-else to="/addAddress/createMnemonic">
+        <template #head>
           <Iconfont name="create" size="17" color="#6D8AF3" />
-        </div>
-        <span>{{ $tt('Create Mnemonic') }}</span>
-        <Iconfont class="arrow-right" name="arrow-right" width="12" height="14" color="#D8D8D8" />
-      </div>
-      <div class="key-item-box" style="border-radius: 12px 12px 0px 0px;" @click="$router.push(`/import/importPrivateKey/${$route.params.key}`)">
-        <div>
+        </template>
+
+        {{ $tt('Create Mnemonic') }}
+      </BoxItem>
+    </BoxList>
+
+    <BoxList>
+      <BoxItem v-if="hasMnemonic" :to="`/import/importPrivateKey/${$route.params.key}`">
+        <template #head>
           <Iconfont name="key" size="17" color="#8E98B2" />
-        </div>
-        <span>{{ $tt('Import by priority key') }}</span>
-        <Iconfont class="arrow-right" name="arrow-right" width="12" height="14" color="#D8D8D8" />
-      </div>
-      <div v-if="!hasMnemonic" class="key-item-box" style="border-radius: 0px 0px 12px 12px;" @click="$router.push('/import/importMnemonic')">
-        <div>
+        </template>
+        {{ $tt('Import by priority key') }}
+      </BoxItem>
+
+      <BoxItem v-if="!hasMnemonic" to="/import/importMnemonic">
+        <template #head>
           <Iconfont name="mnemonic" size="17" color="#B2EED7" />
-        </div>
-        <span>{{ $tt('Import by Mnemonic') }}</span>
-        <Iconfont class="arrow-right" name="arrow-right" width="12" height="14" color="#D8D8D8" />
-      </div>
+        </template>
+        {{ $tt('Import by Mnemonic') }}
+      </BoxItem>
       <!-- <div class="key-item-box" style="border-radius: 0px 0px 12px 12px;" @click="$router.push(`/importKeystoreFile/${$route.params.key}`)">
         <div>
           <Iconfont name="keystore" size="17" color="#FFCC59"></Iconfont>
@@ -84,7 +53,7 @@
         <span>{{ $tt('Connect hard wallet') }}</span>
         <Iconfont class="arrow-right" name="arrow-right" width="12" height="14" color="#D8D8D8"></Iconfont>
       </div> -->
-    </div>
+    </BoxList>
     <Ironman />
   </div>
 </template>
@@ -94,9 +63,15 @@ import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import type { KeyIdentifier } from '~/constants'
 import { wallet } from '~/ui/controllers/wallet'
+import BoxList from '~/ui/components/Box/BoxList.vue'
+import BoxItem from '~/ui/components/Box/BoxItem.vue'
 
 export default {
   name: 'PageAddAddress',
+  components: {
+    BoxList,
+    BoxItem,
+  },
   setup () {
     const hasMnemonic = ref(true)
     onMounted(async () => {
