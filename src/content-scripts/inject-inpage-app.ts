9f2d04630774248aaeb48ea9ce3651d8d5a656fa
Jeff Jing <zgayjjf@qq.com>
2022-03-01 16:41:08 +0800

    chore: add github release action (#69)
    * chore: use lib/sign.mjs instead of dist/sign.mjs
    
    * feat: only inject inpage under development

--- src/content-scripts/inject-inpage-app.ts
+++ src/content-scripts/inject-inpage-app.ts
@@ -1,16 +1,14 @@
 import { createApp } from 'vue'
 import App from '~/ui/inpage/App.vue'
 
-// todo: only inject test component under development
-// if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
+  // mount component to context window
+  const container = document.createElement('div')
+  const root = document.createElement('div')
 
-// mount component to context window
-const container = document.createElement('div')
-const root = document.createElement('div')
+  const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
 
-const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
-
-shadowDOM.appendChild(root)
-document.body.appendChild(container)
-createApp(App).mount(root)
-// }
+  shadowDOM.appendChild(root)
+  document.body.appendChild(container)
+  createApp(App).mount(root)
+}
